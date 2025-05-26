74f6eca410e28b9c90a8f016728d068a1856136a
adal4ik <adilet.adilek@gmail.com>
2025-05-26 22:34:04 +0500

    feat(main): add main entrypoint with database connection

--- cmd/task-manager/main.go
+++ cmd/task-manager/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"context"
+	"task-manager/internal/adapters/driven/database"
+	"task-manager/internal/config"
+)
 
+func main() {
+	cfg := config.Load()
+	ctx := context.Background()
+	db := database.ConnectDB(ctx, cfg.Database)
+	defer db.Close()
 }
