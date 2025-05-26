4e4151479f735734b8e00c820369be4750f6b7d9
adal4ik <adilet.adilek@gmail.com>
2025-05-26 22:26:45 +0500

    feat(database): implemet PostgreSQL connection with config

--- internal/adapters/driven/database/conn.go
+++ internal/adapters/driven/database/conn.go
@@ -1,1 +1,33 @@
 package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"task-manager/internal/config"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib" // Import the pgx driver for PostgreSQL
+)
+
+func ConnectDB(ctx context.Context, cfg config.DatabaseConfig) *sql.DB {
+	time.Sleep(5 * time.Second)
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Name,
+	)
+
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		log.Fatalf("Failed to open DB: %v", err)
+	}
+
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatalf("Database ping failed: %v", err)
+	}
+
+	log.Println("Successfully connected to the database!")
+	return db
+}
