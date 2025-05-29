8a57d860f993fb1623cf2e5bb53fadcbf070cfec
adal4ik <adilet.adilek@gmail.com>
2025-05-30 00:08:20 +0500

    feat(http): add chi router

--- internal/adapters/driver/http/router.go
+++ internal/adapters/driver/http/router.go
@@ -1,1 +1,13 @@
 package web
+
+import (
+	"task-manager/internal/adapters/driver/http/handlers"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Router(handler handlers.Handler) *chi.Mux {
+	r := chi.NewRouter()
+	r.HandleFunc("POST /register", handler.RegisterHandler.RegisterUser)
+	return r
+}
