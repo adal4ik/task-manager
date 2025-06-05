6781642eac96bc6f5015e154f2f2c8207f57502e
adal4ik <adilet.adilek@gmail.com>
2025-06-05 22:00:08 +0500

    feat(middleware): add token auth

--- internal/adapters/driver/http/middleware/middleware.go
+++ internal/adapters/driver/http/middleware/middleware.go
@@ -1,1 +1,61 @@
 package middleware
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type contextKey string
+
+const userIDKey contextKey = "userID"
+
+func AuthenticateJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		authHeader := req.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Missing Authorization header", http.StatusUnauthorized)
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			http.Error(w, "Invalid Authorization format", http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := parts[1]
+
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte("your_secret_key"), nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			http.Error(w, "Invalid token claims", http.StatusUnauthorized)
+			return
+		}
+
+		userID, ok := claims["user_id"].(string)
+		if !ok {
+			http.Error(w, "user_id missing in token", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(req.Context(), userIDKey, userID)
+		req = req.WithContext(ctx)
+
+		next.ServeHTTP(w, req)
+	})
+}
